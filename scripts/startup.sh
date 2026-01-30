fb34f6f0f09a201c32b3bc411bf1ac5eb29fc221
wadayym <35057133+wadayym@users.noreply.github.com>
2026-01-30 14:14:21 +0000

    startup.sh を再度修正

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt update && sudo apt install libxcb1
+apt update && sudo apt install libxcb1
 python -m pip install -r requirements.txt
 gunicorn --bind=0.0.0.0 --timeout 600 application:app
