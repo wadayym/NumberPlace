5d0e01202ecade4fddeeca600d691bf4dd9f851a
wadayym <35057133+wadayym@users.noreply.github.com>
2026-01-30 12:22:25 +0000

    startup.sh を再度修正

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update && sudo apt install libxcb1
-#!python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 #!python -m pip show Pillow
 #!python -c "import PIL; print(PIL.__version__)"
 gunicorn --bind=0.0.0.0 --timeout 600 application:app
